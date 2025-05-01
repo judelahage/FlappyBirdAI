6681f4cc995d070989d8711221222435d5777759
judelahage <106047025+judelahage@users.noreply.github.com>
2025-05-01 02:23:09 -0400

    made jump and move method for bird class

--- flappy_bird.py
+++ flappy_bird.py
@@ -31,5 +31,30 @@ class Bird:
         self.imgCount = 0 #this is for knowing which image in the animation the bird is currently showing
         self.img = self.IMGS[0] #this sets the first default image of the bird to be the first one in the list of animation images
     
-    
-    
+    def jump(self):
+        self.vel = -10.5 #velocity is negative becuase 0,0 is the top left of the screen, so in order to move up, there has to be a negative velocity
+        self.tickCount  = 0 #tick count trracks when the bird last jumped
+        self.height = self.y #we need to store the original height of the bird right before it jumps
+        
+    def move(self): #records one "move" of a bird in a frame
+        self.tickCount +=1 #tick count increases by 1
+        displacement = self.vel * self.tickCount + 1.5 * self.tickCount**2 #records the amount of distance that the bird moves in the frame
+        
+        #we have to implement a terminal velocity however, we don't want the velocity to get infinitely large
+        if d >= 16: #if d gets to 16, then we keep it at 16
+            d = 16; 
+
+        if(d<0): #if d is negative, meaning were jumping, then that means that we make it more negative so the jumps get stronger and stronger
+            d -= 2
+            
+        self.y += d #the y just hanges by d, and the bird only moves in the y direction, the background and tubes will move in x direction
+        
+        if d < 0 or self.y < self.height + 50: #basically we have to see, in the middle of the jump, if the bird is currently above the original y coordinate from where it jumped, then it should be tilted up
+            if self.tilt < self.MAXROTATION:
+                self.tilt = self.MAXROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROTVEL
+            
+            
+        
